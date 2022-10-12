b3bd4e6a0a12a3f9b515bd8d4b7a53d66ad9876d
Alv4r0Dev13 <alvarorenancf00@gmail.com>
2022-10-12 16:23:50 -0300

    enviando exercícios BM02 03 e 04

---
+++ BMExercicio04.java
@@ -0,0 +1,11 @@
+public class BMExercicio04 {
+    public static void main(String[] args) {
+        long long1 = 100;
+        int int1 = 200;
+        int int2 = int1 + (int) long1;
+
+        // A soma não pode ser realizada, pois, os valores são de tipos distintos
+
+        System.out.println(int2);
+    }
+}
